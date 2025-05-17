ea3135e2a4a4496adc4d8437643dac14c20992c9
Shinsei <murakami.shinsei.zn@gmail.com>
2025-05-18 00:53:42 +0900

    wip

--- api/scheduled.ts
+++ api/scheduled.ts
@@ -1,15 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { sendLine } from '../src/app/lib/line';
 import { getTodayTasks } from '../src/app/lib/notion';
 
-export default async (req: any, res: any) => {
-  if (req.method !== 'GET') {
-    res.status(405).send('Method Not Allowed');
-    return;
-  }
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   const tasks = await getTodayTasks();
   if (tasks.length > 0) {
     const message = tasks.map((t) => `🔔 ${t}`).join('\n');
     await sendLine(message);
   }
-  res.status(200).send('OK');
-};
+  response.status(200).json({ message: 'OK' });
+}
